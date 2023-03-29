3d006244228dee28562b46062f78891d5ed175b7
Amanda Wright <akhawkins1@hotmail.com>
2023-03-29 14:03:40 -0400

    plumbers unite

---
+++ src/scripts/Requests.js
@@ -0,0 +1,92 @@
+import { getRequests, getPlumbers, saveCompletion } from "./dataAccess.js"
+
+
+
+
+
+
+const convertToListElement = (request,plumbers) => {
+    return`
+    <li>
+    ${request.description}
+    <select class="plumbers" id="plumbers">
+    <option value="">Choose</option>
+    ${
+        plumbers.map(
+            plumber => {
+                return `<option value="${request.id}--${plumber.id}">${plumber.name}</option>`
+            }
+        ).join("")
+    }
+</select>
+    <button class="request__delete"
+    id="request--${request.id}">
+Delete
+</button>
+</li>
+
+`
+}
+
+
+
+
+export const Requests = () => {
+    const requests = getRequests()
+const plumbers = getPlumbers()
+
+    let html =  requests.map (request => convertToListElement(request, plumbers)).join("")
+           
+          
+
+   return html
+}
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [,requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
+
+
+
+
+
+
+
+mainContainer.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.id === "plumbers") {
+            const [requestId, plumberId] = event.target.value.split("--")
+
+            /*
+                This object should have 3 properties
+                   1. requestId
+                   2. plumberId
+                   3. date_created
+            */
+            const completion = {
+                requestId :requestId,
+                plumberId : plumberId,
+                date_created : Date.now()
+            }
+
+            /*
+                Invoke the function that performs the POST request
+                to the `completions` resource for your API. Send the
+                completion object as a parameter.
+             */
+            saveCompletion(completion)
+        }
+    }
+)
+
+
+
+
+
+
+
